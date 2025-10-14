e7841294173d35ee76574b90ec7a59a232b5a1f4
MarsEndeavor <131321814+DiegoMars@users.noreply.github.com>
2025-10-13 18:39:25 -0700

    Temp add new colorscheme

--- lua/lazy/plugins/colorscheme.lua
+++ lua/lazy/plugins/colorscheme.lua
@@ -1,6 +1,20 @@
+-- Need a different colorscheme, or make one with this tool eventually
+-- return {
+--   "tjdevries/colorbuddy.nvim",
+--   config = function()
+--     vim.cmd.colorscheme("gruvbuddy")
+--   end
+-- }
+
 return {
-  "tjdevries/colorbuddy.nvim",
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  opts = {
+    flavour = "mocha",
+  },
   config = function()
-    vim.cmd.colorscheme("gruvbuddy")
+    vim.cmd.colorscheme "catppuccin"
   end
 }
+
