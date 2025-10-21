90548f087d57b0ec96ada2e71e30af43c6e108ad
MarsEndeavor <131321814+DiegoMars@users.noreply.github.com>
2025-10-20 17:39:58 -0700

    Added working java lsp stuff

--- lua/lazy/plugins/nvim-lsp.lua
+++ lua/lazy/plugins/nvim-lsp.lua
@@ -141,6 +141,7 @@ return {
       rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       astro = {},
+      jdtls = {},
       lua_ls = {
         -- cmd = { ... },
         -- filetypes = { ... },
