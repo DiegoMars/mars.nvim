108a7b737f687ffc4b17d6cc6814ccd524637efa
Diegos_Rad <131321814+DiegoMars@users.noreply.github.com>
2026-01-30 21:13:09 -0800

    Added python3

--- lua/mars/languages/py.lua
+++ lua/mars/languages/py.lua
@@ -2,7 +2,12 @@ return {
   vim.api.nvim_create_autocmd("FileType", {
     pattern = { "python" },
     callback = function()
-      vim.keymap.set('n', '<Leader>r', ':sp | term python %<CR>', { buffer = true, desc = '[R]un Python' })
+      require("which-key").add({
+        { "<leader>r", group = "[r]un java" }
+      })
+
+      vim.keymap.set('n', '<Leader>rr', ':sp | term python %<CR>', { buffer = true, desc = '[R]un Python' })
+      vim.keymap.set('n', '<Leader>rt', ':sp | term python3 %<CR>', { buffer = true, desc = '[R]un Python3' })
     end,
   }),
 }
