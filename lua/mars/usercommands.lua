2ad8c141ef34e2c27b16ec82c97e2baec5e66d87
DiegoMars <131321814+DiegoMars@users.noreply.github.com>
2026-03-25 11:40:50 -0700

    Added diagnostic toggle

--- lua/mars/usercommands.lua
+++ lua/mars/usercommands.lua
@@ -5,3 +5,13 @@ vim.api.nvim_create_user_command(
     desc = 'LspInfo'
   }
 )
+
+vim.api.nvim_create_user_command(
+  'DT', -- DiagnosticsToggle
+  function()
+    vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+  end,
+  {
+    desc = 'Toggle diagnostics on/off'
+  }
+)
