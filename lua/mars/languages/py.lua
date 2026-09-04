47a348392cfa3861af3b92c027f59a5c4dfc2a7f
Diego Mars <131321814+DiegoMars@users.noreply.github.com>
2026-09-03 22:59:05 -0700

    Working on python config

--- lua/mars/languages/py.lua
+++ lua/mars/languages/py.lua
@@ -1,15 +1,27 @@
-return {}
--- Need to update
--- return {
---   vim.api.nvim_create_autocmd("FileType", {
---     pattern = { "python" },
---     callback = function()
---       require("which-key").add({
---         { "<leader>r", group = "[r]un java" }
---       })
---
---       vim.keymap.set('n', '<Leader>rr', ':sp | term python %<CR>', { buffer = true, desc = '[R]un Python' })
---       vim.keymap.set('n', '<Leader>rt', ':sp | term python3 %<CR>', { buffer = true, desc = '[R]un Python3' })
---     end,
---   }),
--- }
+return {
+  name = 'Python',
+  filetypes = { 'python' },
+  -- Currently, this give me a bunch of weird errors
+  -- mason_tools = { 'basedpyright' },
+  -- servers = {
+  --   basedpyright = {
+  --     analysis = {
+  --       autoSearchPaths = true,
+  --       diagnosticMode = "openFilesOnly",
+  --       useLibraryCodeForTypes = true,
+  --
+  --       -- This is the default for pyright. Read more about it here:
+  --       -- https://docs.basedpyright.com/latest/configuration/language-server-settings/#neovim:~:text=basedpyright.analysis.-,typeCheckingMode,-%5B%22off%22%2C%20%22basic%22%2C%20%22standard
+  --       typeCheckingMode = "recommended"
+  --     }
+  --   },
+  -- },
+  on_filetype = function(bufnr)
+    require("which-key").add({
+      { "<leader>r", group = "[r]un python" }
+    })
+
+    vim.keymap.set('n', '<Leader>rr', ':sp | term python %<CR>', { buffer = true, desc = '[R]un Python' })
+    vim.keymap.set('n', '<Leader>rt', ':sp | term python3 %<CR>', { buffer = true, desc = '[R]un Python3' })
+  end,
+}
